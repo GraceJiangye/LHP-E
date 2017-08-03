821dd23002843073139331408df3d61002e000e9
Jiangye Ma <30498197+GraceJiangye@users.noreply.github.com>
2017-08-04 03:51:17 +0800

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,28 @@
+#-*-coding:utf-8-*-
+# A Game like this is not fun at all.
+print "I will now count my chickens:"
+
+# This can not be a comment typing in Chinese. It is not cool.
+print "Hens", 25 + 30 /6
+
+# 符号的优先级需要注意，%的含义并非百分比，而是取余（M），非常久远的算数逻辑
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
